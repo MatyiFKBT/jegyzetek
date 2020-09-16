321ef45891b0168a728889db9f9d6980f3439ed1
MatyiFKBT <mmatyi1999@gmail.com>
2020-09-16 16:14:06 +0200

    docs: stackedit (#2)

---
+++ docs/konk/gyak2/src/kindergarten/view/Gizi.java
@@ -0,0 +1,7 @@
+package kindergarten.view;
+import java.io.IOException;
+import java.io.InputStream;
+
+public interface Gizi {
+	public String prepareData(InputStream inputStream) throws IOException;
+}
